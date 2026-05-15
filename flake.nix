2af6b2b6ca5c3ee63f1ed194745220065f5ea3fd
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-05-15 13:20:28 +0200

    nix: clean
    - https://github.com/Gepetto/nix/pull/311 was merged
    - set QT_PLUGIN_PATH in dev shell

--- flake.nix
+++ flake.nix
@@ -2,70 +2,25 @@
   description = "Flake for inspire_hand_ws with unitree_sdk2_python";
 
   inputs = {
-    gepetto.url = "github:gepetto/nix/pull/311/merge";
+    gepetto.url = "github:gepetto/nix";
   };
 
   outputs =
     inputs:
     inputs.gepetto.lib.mkFlakoboros inputs (
-      { ... }:
+      { lib, ... }:
       {
-        extraPyPackages = [ "pymodbus" ];
-        pyPackages.pymodbus_369 =
+        pyOverrideAttrs.inspire-hand-sdk =
+          { pkgs-final, ... }:
           {
-            pymodbus,
-            fetchFromGitHub,
-          }:
-          pymodbus.overrideAttrs (
-            drv-final: drv-prev: {
-              version = "3.6.9";
-              src = fetchFromGitHub {
-                inherit (drv-prev.src) owner repo;
-                tag = "v${drv-final.version}";
-                hash = "sha256-ScqxDO0hif8p3C6+vvm7FgSEQjCXBwUPOc7Y/3OfkoI=";
-              };
-              disabledTestPaths = [ ];
-            }
-          );
-        pyPackages.inspire-hand-ws =
-          {
-            lib,
-            buildPythonPackage,
-            setuptools,
-            cyclonedds-python_10,
-            numpy,
-            pyqt5,
-            pyqtgraph,
-            qt5,
-            colorcet,
-            pymodbus_369,
-            pyserial,
-            unitree-sdk2-python,
-          }:
-          buildPythonPackage (_finalAttrs: {
-            name = "inspire-hand-sdk";
-            version = "0-unstable-2026-05-07";
-            pyproject = true;
             src = lib.cleanSource ./inspire_hand_sdk;
-            build-system = [ setuptools ];
-            dependencies = [
-              cyclonedds-python_10
-              numpy
-              pyqt5
-              pyqtgraph
-              colorcet
-              pymodbus_369
-              pyserial
-              unitree-sdk2-python
+
+            env.QT_PLUGIN_PATH = lib.makeSearchPathOutput "bin" pkgs-final.qt5.qtbase.qtPluginPrefix [
+              pkgs-final.qt5.qtbase
+              pkgs-final.qt5.qtdeclarative
+              pkgs-final.qt5.qtwayland
             ];
-            pythonRelaxDeps = [ "pymodbus" ];
-            pythonImportsCheck = [ "inspire_sdkpy" ];
-            passthru.qt-env = lib.makeSearchPathOutput "bin" qt5.qtbase.qtPluginPrefix [
-              qt5.qtbase
-              qt5.qtdeclarative
-              qt5.qtwayland
-            ];
-          });
+          };
       }
     );
 }
